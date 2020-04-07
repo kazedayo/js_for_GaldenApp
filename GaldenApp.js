c9c2e7ef8aeff1926d263fe5406c5c5a75c873c7
Lam Kin Wa <winsonlam@me.com>
2020-04-07 21:57:39 +0800

    Update GaldenApp.js

--- GaldenApp.js
+++ GaldenApp.js
@@ -22,3 +22,11 @@ $(window).scroll(function() {
     	}
 	});
 });
+
+function fix() {
+	var el = this;
+	var par = el.parentNode;
+	var next = el.nextSibling;
+	par.removeChild(el);
+	setTimeout(function() {par.insertbefore(el, next);}, 0)
+}
